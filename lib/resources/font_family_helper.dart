c2a14d4b505c60ba41e1dc8590d6e1dc411609d8
ibraheem_aletrebi <ibraheemaletrebi@gmail.com>
2026-03-12 23:42:55 +0200

    add font family helper

---
+++ lib/resources/font_family_helper.dart
@@ -0,0 +1,3 @@
+class FontFamilyHelper {
+  static const String alexandria = 'Alexandria';
+}
--- lib/resources/font_weight_helper.dart
+++ lib/resources/font_weight_helper.dart
@@ -1,4 +1,3 @@
-// lib/core/resources/font_weight_helper.dart
 import 'package:flutter/material.dart';
 
 class FontWeightHelper {
