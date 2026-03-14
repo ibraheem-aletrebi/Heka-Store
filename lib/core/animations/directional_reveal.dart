0e1145838cb4879792a4d554ba0baa4c710574fc
ibraheem_aletrebi <ibraheemaletrebi@gmail.com>
2026-03-14 06:49:52 +0200

    add directional reveal animation

---
+++ lib/core/animations/directional_reveal.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+class DirectionalReveal extends StatelessWidget {
+  final double progress;
+  final bool isRtl;
+  final Widget child;
+
+  const DirectionalReveal({
+    super.key,
+    required this.progress,
+    required this.child,
+    this.isRtl = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: Align(
+        alignment: isRtl ? Alignment.centerRight : Alignment.centerLeft,
+        widthFactor: progress,
+        child: child,
+      ),
+    );
+  }
+}
