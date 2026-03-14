032d8427f2af849de3c5e613ac50b77bf26fc44a
ibraheem_aletrebi <ibraheemaletrebi@gmail.com>
2026-03-14 06:18:24 +0200

    add get it package for service locator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:heka_store/main.dart';
+import 'package:heka_store/app/heka_store_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
