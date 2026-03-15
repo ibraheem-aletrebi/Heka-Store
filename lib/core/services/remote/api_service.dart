91233dc68d838312e414e13048a17ff5d7fed6cc
ibraheem_aletrebi <ibraheemaletrebi@gmail.com>
2026-03-15 04:49:42 +0200

    add api service

---
+++ lib/core/services/remote/api_service.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio;
+
+  ApiService(this.dio);
+  Future<Response> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return dio.get(path, queryParameters: queryParameters, options: options);
+  }
+
+  Future<Response> post(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return dio.post(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response> put(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return dio.put(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response> delete(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return dio.delete(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+}
