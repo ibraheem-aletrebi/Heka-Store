f319b2a61899524372b7c7900898e76ee6a68ab0
ibraheem_aletrebi <ibraheemaletrebi@gmail.com>
2026-03-15 04:54:29 +0200

    add api result to handle failure and success state , add api error handler to check error and create api error model to return it in failure state , api error messages contain error key and translation ar , en

---
+++ lib/core/services/remote/api_result.dart
@@ -0,0 +1,47 @@
+
+
+import 'package:heka_store/core/services/remote/api_error_handler.dart';
+import 'package:heka_store/core/services/remote/error/api_error_model.dart';
+
+abstract class ApiResult<T> {
+  const ApiResult();
+
+  R when<R>({
+    required R Function(T data) onSuccess,
+    required R Function(ApiErrorModel error) onError,
+  });
+
+  factory ApiResult.success(T data) = ApiSuccess<T>;
+  factory ApiResult.error(dynamic error) = ApiError<T>;
+}
+
+class ApiSuccess<T> extends ApiResult<T> {
+  final T data;
+
+  const ApiSuccess(this.data);
+
+  @override
+  R when<R>({
+    required R Function(T data) onSuccess,
+    required R Function(ApiErrorModel error) onError,
+  }) {
+    return onSuccess(data);
+  }
+}
+
+class ApiError<T> extends ApiResult<T> {
+  final dynamic error;
+  late final ApiErrorModel errorModel;
+
+  ApiError(this.error) {
+    errorModel = ApiErrorHandler().handle(error);
+  }
+
+  @override
+  R when<R>({
+    required R Function(T data) onSuccess,
+    required R Function(ApiErrorModel error) onError,
+  }) {
+    return onError(errorModel);
+  }
+}
