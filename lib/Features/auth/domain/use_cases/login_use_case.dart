32c1af8a83d52e671b9b6adfbc85b923a54acd9d
ibraheem_aletrebi <ibraheemaletrebi@gmail.com>
2026-03-16 06:27:22 +0200

    add login use case

---
+++ lib/Features/auth/domain/use_cases/login_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:heka_store/Features/auth/data/models/login/request/login_request_model.dart';
+import 'package:heka_store/Features/auth/data/models/login/response/login_response_model.dart';
+import 'package:heka_store/Features/auth/domain/repos/auth_repo.dart';
+import 'package:heka_store/core/services/remote/api_result.dart';
+import 'package:heka_store/core/use_cases/use_cases.dart';
+
+class LoginUseCase extends UseCase<LoginResponseModel, LoginRequestModel> {
+  final AuthRepo authRepo;
+  LoginUseCase({required this.authRepo});
+
+  @override
+  Future<ApiResult<LoginResponseModel>> call([LoginRequestModel? param]) =>
+      authRepo.login(loginRequestModel: param!);
+}
