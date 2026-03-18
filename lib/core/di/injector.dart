4d668caaef9bbebd908cb19bcec837cf3c640052
ibraheem_aletrebi <ibraheemaletrebi@gmail.com>
2026-03-18 06:29:21 +0200

    add injector of register

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -5,15 +5,19 @@ import 'package:heka_store/Features/auth/domain/repos/auth_repo.dart';
 import 'package:heka_store/Features/auth/domain/use_cases/login/login_use_case.dart';
 import 'package:heka_store/Features/auth/domain/use_cases/forgot_password/forgot_password_use_case.dart';
 import 'package:heka_store/Features/auth/domain/use_cases/forgot_password/reset_password_use_case.dart';
-import 'package:heka_store/Features/auth/domain/use_cases/resend_otp_use_case.dart';
 import 'package:heka_store/Features/auth/domain/use_cases/forgot_password/verify_reset_otp_use_case.dart';
+import 'package:heka_store/Features/auth/domain/use_cases/register/register_use_case.dart';
+import 'package:heka_store/Features/auth/domain/use_cases/register/verify_email_otp_use_case.dart';
+import 'package:heka_store/Features/auth/domain/use_cases/resend_otp_use_case.dart';
 import 'package:heka_store/Features/auth/presentation/blocs/login/login_bloc.dart';
 import 'package:heka_store/Features/auth/presentation/blocs/forgot_password/forgot_password_bloc.dart';
+import 'package:heka_store/Features/auth/presentation/blocs/register/register_bloc.dart';
 import 'package:heka_store/core/app/router/app_router.dart';
 import 'package:heka_store/core/blocs/language/language_bloc.dart';
 import 'package:heka_store/core/blocs/theme/theme_bloc.dart';
 import 'package:heka_store/core/enums/app_theme_mode_enum.dart';
 import 'package:heka_store/core/services/local/local_storage_service.dart';
+import 'package:heka_store/core/services/local/secure_storage_service.dart';
 import 'package:heka_store/core/services/remote/api_service.dart';
 import 'package:heka_store/core/services/remote/dio_client.dart';
 import 'package:heka_store/core/services/remote/error/api_error_handler.dart';
@@ -25,26 +29,37 @@ Future<void> setupInjector() async {
   _initAuth();
 }
 
+// ─── Core ─────────────────────────────────────────────────────────────────────
+
 Future<void> _initCore() async {
-  // Local Storage
+  // ─── Local Storage ────────────────────────────────
   sl.registerLazySingleton<LocalStorageService>(() => LocalStorageService());
   final localStorage = sl<LocalStorageService>();
   await localStorage.init(adapters: [AppThemeModeEnumAdapter()]);
 
-  // Router
+  // ─── Secure Storage ───────────────────────────────
+  sl.registerLazySingleton<SecureStorageService>(
+    () => SecureStorageService(),
+  );
+
+  // ─── Router ───────────────────────────────────────
   sl.registerLazySingleton<AppRouter>(() => AppRouter());
 
-  // App BLoCs
-  sl.registerFactory<ThemeBloc>(() => ThemeBloc(localStorage: localStorage));
+  // ─── App BLoCs ────────────────────────────────────
+  sl.registerFactory<ThemeBloc>(
+    () => ThemeBloc(localStorage: localStorage),
+  );
   sl.registerFactory<LanguageBloc>(
     () => LanguageBloc(localStorage: localStorage),
   );
 
-  // Network
+  // ─── Network ──────────────────────────────────────
   DioClient().init();
-  sl.registerLazySingleton<ApiService>(() => ApiService(DioClient().dio));
+  sl.registerLazySingleton<ApiService>(
+    () => ApiService(DioClient().dio),
+  );
 
-  // Error Handler
+  // ─── Error Handler ────────────────────────────────
   ApiErrorHandler.instance.init(
     onUnauthorized: () {
       // TODO: Navigate to Login
@@ -52,20 +67,36 @@ Future<void> _initCore() async {
   );
 }
 
+// ─── Auth ─────────────────────────────────────────────────────────────────────
+
 void _initAuth() {
-  // DataSources
+  // ─── DataSources ──────────────────────────────────
   sl.registerLazySingleton<AuthRemoteDataSource>(
     () => AuthRemoteDataSourceImpl(apiService: sl<ApiService>()),
   );
+  // sl.registerLazySingleton<AuthLocalDataSource>(
+  //   () => AuthLocalDataSourceImpl(
+  //     secureStorage: sl<SecureStorageService>(),
+  //     localStorage: sl<LocalStorageService>(),
+  //   ),
+  // );
 
-  //Repository
+  // ─── Repository ───────────────────────────────────
   sl.registerLazySingleton<AuthRepo>(
-    () => AuthRepoImp(remoteDataSource: sl<AuthRemoteDataSource>()),
+    () => AuthRepoImp(
+      remoteDataSource: sl<AuthRemoteDataSource>(),
+    ),
   );
 
-  //Use Cases
+  // ─── Use Cases ────────────────────────────────────
   sl.registerFactory<LoginUseCase>(
     () => LoginUseCase(authRepo: sl<AuthRepo>()),
+  );
+  sl.registerFactory<RegisterUseCase>(
+    () => RegisterUseCase(authRepo: sl<AuthRepo>()),
+  );
+  sl.registerFactory<VerifyEmailOtpUseCase>(
+    () => VerifyEmailOtpUseCase(authRepo: sl<AuthRepo>()),
   );
   sl.registerFactory<ForgotPasswordUseCase>(
     () => ForgotPasswordUseCase(repository: sl<AuthRepo>()),
@@ -80,9 +111,12 @@ void _initAuth() {
     () => ResetPasswordUseCase(repository: sl<AuthRepo>()),
   );
 
-  //BLoCs
+  // ─── BLoCs ────────────────────────────────────────
   sl.registerFactory<LoginBloc>(
     () => LoginBloc(loginUseCase: sl<LoginUseCase>()),
+  );
+  sl.registerFactory<RegisterBloc>(
+    () => RegisterBloc(registerUseCase: sl<RegisterUseCase>()),
   );
   sl.registerFactory<ForgotPasswordBloc>(
     () => ForgotPasswordBloc(
@@ -92,4 +126,6 @@ void _initAuth() {
       resetPasswordUseCase: sl<ResetPasswordUseCase>(),
     ),
   );
+
+  
 }
